e8149cc01fdf6a9922bb14f8ae16763761820f1c
xiTTzP <es.vito@hotmail.com>
2024-11-01 19:08:39 -0300

    Adicionado armazenamento em cache no site e algumas melhorias no código css para deixar mais limpo

--- galeria-html/galeria.js
+++ galeria-html/galeria.js
@@ -1,2 +1,10 @@
-
-
+const imagensArmazenar = document.querySelectorAll("img");
+//Salva as imagens da galeria no cache do navegador, reduzindo o consumo de banda larga.
+caches.open("cache-img-galeria").then(cache => {
+    imagensArmazenar.forEach(img => {
+        const url = img.src;
+        fetch(url).then(response => {
+            cache.put(url, response)
+        })
+    })
+})
